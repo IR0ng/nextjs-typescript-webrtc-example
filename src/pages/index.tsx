90463fa3144f44e318ac1a9af12ede0a07614fc4
IR0ng <zhouirong0420@gmail.com>
2024-05-28 16:23:36 +0800

    Testing iceCandidates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
+import { io } from "socket.io-client";
 
 export default function Home() {
+  let pc: RTCPeerConnection 
+  const socket = io('http://localhost:80');
+
   const [localStream, setLocalStream] = useState<MediaStream | null>(null)
+
   useEffect(() => {
     (async () => {
       const localStream = await navigator.mediaDevices.getUserMedia({
@@ -12,9 +14,32 @@ export default function Home() {
         video: true,
       })
       setLocalStream(localStream)
-      console.log('here')
     })()
   }, [])
+
+  const joinRoom = () => {
+    socket.emit('joinRoom', { room: 'testRoom' })
+    createPeerConnection()
+  }
+
+  const createPeerConnection = () => {
+    const configuration = {
+      iceServers: [
+        {
+            urls: 'stun:stun.l.google.com:19302',
+        }, {
+            urls: 'stun:stun.xten.com',
+        }],
+     };
+    pc = new RTCPeerConnection(configuration);
+    localStream!.getTracks().forEach(track => pc.addTrack(track, localStream!));
+    pc.onicecandidate = ({ candidate }) => {
+      if (!candidate) return
+      console.log('onIceCandidate => ', candidate);
+      socket.emit("connectSignaling", { candidate });
+    };
+  };
+
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-center">
@@ -28,6 +53,14 @@ export default function Home() {
           playsInline
           muted={true}
         />
+        <button 
+          onClick={() => {
+            joinRoom()
+          }}
+          disabled={!localStream}
+        >
+          加入
+        </button>
       </div>
     </div>
   );
