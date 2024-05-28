a156ef123fbf488b48a562aa111a81154d9be5ba
IR0ng <zhouirong0420@gmail.com>
2024-05-28 18:55:40 +0800

    Testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:80', { withCredentials: true });
-
+const socket = io('signal-server-example-production.up.railway.app/', { withCredentials: true, transports: ["websocket"] });
+const room = 'testRoom'
 export default function Home() {
   let pc: RTCPeerConnection 
   const [localStream, setLocalStream] = useState<MediaStream | null>(null)
-
+  const [remoteList, setRemoteList] = useState<MediaStream[]>([])
   useEffect(() => {
     (async () => {
       const localStream = await navigator.mediaDevices.getUserMedia({
@@ -15,11 +15,33 @@ export default function Home() {
       })
       setLocalStream(localStream)
     })()
-    socket.on("broadcast", () => console.log('test'))
   }, [])
 
+  useEffect(() => {
+    socket.on("broadcast", () => console.log('test'))
+    socket.on('connectSignaling', async ({ room, sdp, candidate }) => {
+      console.log('socket signaling res =>', {room, sdp, candidate});
+      try {
+        if (sdp) {
+          console.log('socket sdp => ', sdp);
+          await pc.setRemoteDescription(sdp);
+          if (pc.remoteDescription && pc.remoteDescription.type === 'offer') {
+            const description = await pc.createAnswer();
+            await pc.setLocalDescription(description);
+            socket.emit('connectSignaling', { room, sdp: pc.localDescription });
+        }
+        } else if (candidate) {
+          console.log('socket candidate =>', candidate);
+          pc.addIceCandidate(candidate);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }, [socket])
+
   const joinRoom = () => {
-    socket.emit('joinRoom', { room: 'testRoom' })
+    socket.emit('joinRoom', { room })
     createPeerConnection()
   }
 
@@ -36,33 +58,66 @@ export default function Home() {
     
     localStream!.getTracks().forEach(track => pc.addTrack(track, localStream!));
     pc.onicecandidate = ({ candidate }) => {
-      if (!candidate) return
-      console.log('onIceCandidate => ', candidate);
-      socket.emit("connectSignaling", { candidate });
+      if (candidate) {
+        console.log('pc onIceCandidate => ', candidate);
+        socket.emit("connectSignaling", { room, candidate });
+      }
     };
+    pc.onnegotiationneeded = async () => {
+      try {
+        console.log('offer send');
+        await pc.setLocalDescription(await pc.createOffer());
+        socket.emit('connectSignaling', { room, sdp: pc.localDescription });
+      } catch (err) {
+          console.error(err);
+      }
+    }
+    pc.ontrack = ({ streams }) => {
+      console.log('pc streams', streams);
+      // setRemoteList(remoteList => [ ...remoteList, streams ])
+    }
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center justify-center">
-        <video
-          ref={video => {
-            if (video) {
-              video.srcObject = localStream
-            }
-          }}
-          autoPlay
-          playsInline
-          muted={true}
-        />
-        <button 
-          onClick={() => {
-            joinRoom()
-          }}
-          disabled={!localStream}
-        >
-          加入
-        </button>
+    <div className="h-screen bg-white flex">
+      <div className="flex flex-col flex-1 gap-2">
+        <div className="bg-gray-200 shadow-md p-2">
+          <button 
+            className="border rounded-xl bg-gray-400 px-3 py-1"
+            onClick={() => {
+              joinRoom()
+            }}
+            disabled={!localStream}
+          >
+            加入
+          </button>
+        </div>
+        <div className="flex-row flex-1 border rounded-xl p-2">
+          <video
+            className="h-[250px] rounded-xl"
+            ref={video => {
+              if (video) {
+                video.srcObject = localStream
+              }
+            }}
+            autoPlay
+            playsInline
+            muted={true}
+          />
+          <div>
+            <video
+              className="h-[250px] rounded-xl"
+              ref={video => {
+                if (video) {
+                  video.srcObject = localStream
+                }
+              }}
+              autoPlay
+              playsInline
+              muted={true}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
